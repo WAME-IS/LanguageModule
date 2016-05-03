be77ba8b56d883c8d2127e97796b3991b948b474
Linzee <d.gmiterko@gmail.com>
2016-05-03 15:13:39 +0200

    Router events

--- vendor/wame/RouterModule/registers/LanguageRouteListener.php
+++ vendor/wame/RouterModule/registers/LanguageRouteListener.php
@@ -5,20 +5,24 @@ namespace Wame\LanguageModule\Registers;
 use h4kuna\Gettext\GettextSetup,
 	Nette\Utils\Strings,
 	Wame\RouterModule\Entities\RouterEntity,
-	Wame\RouterModule\Registers\RoutePreprocessor;
+	Wame\RouterModule\Routers\Router;
 
 /**
  * @author Dominik Gmiterko <ienze@ienze.me>
  */
-class LanguageRoutePreprocessor implements RoutePreprocessor {
+class LanguageRouteListener {
 
 	const LANG_VAR = "lang";
 
 	/** @var GettextSetup */
 	private $translator;
 
-	public function __construct(GettextSetup $translator) {
+	public function __construct(Router $router, GettextSetup $translator) {
 		$this->translator = $translator;
+		
+		$router->onPreprocess[] = function($event) {
+			$this->process($event->getRoute());
+		};
 	}
 
 	public function process(RouterEntity $entity) {
