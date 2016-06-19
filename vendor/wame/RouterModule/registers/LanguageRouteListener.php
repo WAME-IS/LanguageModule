6b9866eda23ce75059b648d11746b40f4c934ca3
Linzee <d.gmiterko@gmail.com>
2016-06-19 12:33:19 +0200

    FilterListener

--- vendor/wame/RouterModule/registers/LanguageRouteListener.php
+++ vendor/wame/RouterModule/registers/LanguageRouteListener.php
@@ -4,7 +4,7 @@ namespace Wame\LanguageModule\Registers;
 
 use h4kuna\Gettext\GettextSetup,
 	Nette\Utils\Strings,
-	Wame\RouterModule\Entities\RouterEntity,
+	Wame\RouterModule\Routers\ActiveRoute,
 	Wame\RouterModule\Routers\Router;
 
 /**
@@ -19,17 +19,19 @@ class LanguageRouteListener {
 
 	public function __construct(Router $router, GettextSetup $translator) {
 		$this->translator = $translator;
-		
+
 		$router->onPreprocess[] = function($event) {
 			$this->process($event->getRoute());
 		};
 	}
 
-	private function process(RouterEntity $entity) {
+	private function process(ActiveRoute $entity) {
 		if (Strings::contains($entity->route, "<" . self::LANG_VAR . ">")) {
 			$entity->route = str_replace("<" . self::LANG_VAR . ">", "<" . self::LANG_VAR . " " . $this->translator->routerAccept() . ">", $entity->route);
-			if (!$entity->getDefault('lang')) {
-				$entity->setDefault('lang', $this->translator->getDefault());
+			if (!isset($entity->defaults['lang'])) {
+				$d = $entity->defaults;
+				$d['lang'] = $this->translator->getDefault();
+				$entity->defaults = $d;
 			}
 		}
 	}
