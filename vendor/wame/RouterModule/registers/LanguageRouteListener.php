9b07ff0068423903b6aceb92b02874f0facb0871
Linzee <d.gmiterko@gmail.com>
2016-07-28 17:52:01 +0200

    FIX using of router with kdyby/console

--- vendor/wame/RouterModule/registers/LanguageRouteListener.php
+++ vendor/wame/RouterModule/registers/LanguageRouteListener.php
@@ -3,37 +3,42 @@
 namespace Wame\LanguageModule\Vendor\Wame\RouterModule\Registers;
 
 use h4kuna\Gettext\GettextSetup,
-	Nette\Utils\Strings,
-	Wame\RouterModule\Routers\ActiveRoute,
-	Wame\RouterModule\Routers\Router;
+    Nette\Utils\Strings,
+    Wame\RouterModule\Routers\ActiveRoute,
+    Wame\RouterModule\Routers\Router;
 
 /**
  * @author Dominik Gmiterko <ienze@ienze.me>
  */
-class LanguageRouteListener {
+class LanguageRouteListener
+{
 
-	const LANG_VAR = "lang";
+    const LANG_VAR = "lang";
 
-	/** @var GettextSetup */
-	private $translator;
+    /** @var GettextSetup */
+    private $translator;
 
-	public function __construct(Router $router, GettextSetup $translator) {
-		$this->translator = $translator;
+    public function __construct(\Nette\DI\Container $container, GettextSetup $translator)
+    {
+        $this->translator = $translator;
 
-		$router->onPreprocess[] = function($event) {
-			$this->process($event->getRoute());
-		};
-	}
+        $router = $container->getByType(Router::class, false);
+        if ($router) {
+            $router->onPreprocess[] = function($event) {
+                $this->process($event->getRoute());
+            };
+        }
+    }
 
-	private function process(ActiveRoute $entity) {
-		if (Strings::contains($entity->route, "<" . self::LANG_VAR . ">")) {
-			$entity->route = str_replace("<" . self::LANG_VAR . ">", "<" . self::LANG_VAR . " " . $this->translator->routerAccept() . ">", $entity->route);
-			if (!isset($entity->defaults['lang'])) {
-				$d = $entity->defaults;
-				$d['lang'] = $this->translator->getDefault();
-				$entity->defaults = $d;
-			}
-		}
-	}
-
+    private function process(ActiveRoute $entity)
+    {
+        if (Strings::contains($entity->route, "<" . self::LANG_VAR . ">")) {
+            $entity->route = str_replace("<" . self::LANG_VAR . ">", "<" . self::LANG_VAR . " " . $this->translator->routerAccept() . ">", $entity->route);
+            if (!isset($entity->defaults['lang'])) {
+                $d = $entity->defaults;
+                $d['lang'] = $this->translator->getDefault();
+                $entity->defaults = $d;
+            }
+        }
+    }
 }
