04c2d1c8d0120b403be9cdfd809a484345ebc163
shaddow <lameruza@gmail.com>
2016-10-01 16:59:27 +0200

    UPDATE Forms

--- forms/LanguageSwitcherFormBuilder.php
+++ forms/LanguageSwitcherFormBuilder.php
@@ -2,14 +2,15 @@
 
 namespace Wame\LanguageModule\Forms;
 
+use Wame\DynamicObject\Forms\BaseForm;
 use Wame\DynamicObject\Forms\BaseFormBuilder;
 
 class LanguageSwitcherFormBuilder extends BaseFormBuilder
 {
 	/** {@inheritDoc} */
-	public function submit($form, $values)
+	public function submit(BaseForm $form, array $values)
 	{
-//		$sortControl = $form->lookup(\Wame\SortModule\Components\SortControl::class);
+//		$sortControl = $form->lookup(SortControl::class);
 //        $sortControl->setSort($values['sort']);
 	}
 	
